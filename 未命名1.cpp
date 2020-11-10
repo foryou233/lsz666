e632149ce6422a27d28061ac4b78555fea407890
first try <2912256590@qq.com>
2020-11-10 17:14:12 +0800

    first try

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -1,11 +1,24 @@
 #include<stdio.h>
 int main(void)
 {
-	float x,i;
-	for(i=5,x=0;i>0;i--){
-		x=(x+1000)/(1+12*1.875/100);
+	int i,j,k=0,sum=0;
+	for(i=500;i>1;i--){
+		for(j=i-1;j>0;j--){
+			if(i%j==0){
+				break;
+			}
+		}
+			if(j==1){
+				printf("%6d",i);
+				sum=sum+i;
+				k++;
+			}
+		if(k==10){
+			break;
+		}
 	}
-	printf("He must save %.2f at the first year.\n",x);
+	printf("\n sum=%d\n",sum);
+	
     return 0;
  }
 
